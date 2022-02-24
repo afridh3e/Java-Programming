41b43ec5a41d97954357848b1df1fdd87b1af94a
ASUS <ASUS@LAPTOP-7GCLNKDH>
2022-02-24 11:11:43 +0530

    hi friends

---
+++ Practice/src/AddToNumbers1.java
@@ -0,0 +1,14 @@
+
+public class AddToNumbers1 {
+	public static void main(String[] args)
+	{
+		System.out.println("Type the number your wish");
+		int No1 = 135 ;
+		int No2 = 1992;
+		System.out.println(No1 + " " + No2);
+		int sum = No1 + No2;
+		System.out.println("The Total is :"+" "+ sum);
+		
+	}
+
+}
